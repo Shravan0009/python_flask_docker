b16ecb6a75622ef2597103b5043dbe1ef29675a6
RACINFRA <nikinikhil322@gmail.com>
2023-09-14 21:27:19 +0530

    adding this running flask code to github

--- python1.py
+++ python1.py
@@ -1,3 +1,14 @@
 # This project is to deploy flask project using docker container
 
 print("Hey this is my first project on flask")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0", port= int("3000"), debug=True)
